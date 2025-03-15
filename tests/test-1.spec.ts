9f25216f153604aaf67cc949682898af6cb69b75
Adam Bethlehem <adam.bethlehem1@gmail.com>
2025-03-15 13:13:37 +0200

    Added PR Template and remove dummy test

--- tests/test-1.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.bbc.com/sport/formula1/2023/results');
-  await page.getByRole('button', { name: 'Las Vegas Grand Prix, Las' }).click();
-});
